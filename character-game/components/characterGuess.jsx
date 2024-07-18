4e5371c071e897b3bf10aefd7574d68e7e899d70
Abigail <abigailf03@hotmail.ca>
2024-07-18 13:06:13 -0500

    Added Styles
    styles to names list and background

--- character-game/components/characterGuess.jsx
+++ character-game/components/characterGuess.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import "./characterGuess.css";
 
-export default function CharacterGuess(
-    { character , randomCharacters = [] }
-) {
-
-    console.log("CharacterGuess component props:", { character, randomCharacters });
+export default function CharacterGuess({ character, randomCharacters = [] }) {
+  console.log("CharacterGuess component props:", {
+    character,
+    randomCharacters,
+  });
 
   return (
     <div>
-      {randomCharacters.length > 0 && (
-        <div>
-          <h2>Random Characters:</h2>
-          <ul>
-            {randomCharacters.map((characters) => (
-              <li key={characters.id}>{characters.name}</li>
-            ))}
-          </ul>
-        </div>
-      )}
       {character ? (
-        <div>
-          <li>{character.name}</li>
-          <img src={character.image} alt="rick & morty character" id="character"/>
+        <div id="picture">
+          <img
+            src={character.image}
+            alt="rick & morty character"
+            id="character"
+          />
         </div>
       ) : (
         <p>Loading character...</p>
+      )}
+      <h2>Who am I??</h2>
+      {randomCharacters.length > 0 && (
+        <div>
+          <ul>
+            {randomCharacters.map((characters) => (
+              <button>
+                <li key={characters.id}>{characters.name}</li>
+              </button>
+            ))}
+            <button>
+              {" "}
+              <li>{character.name}</li>
+            </button>
+          </ul>
+        </div>
       )}
     </div>
   );
