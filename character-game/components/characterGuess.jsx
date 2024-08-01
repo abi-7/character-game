12c0449b045c6808c1229d491645ab36ebd042b5
Abigail <abigailf03@hotmail.ca>
2024-08-01 08:43:49 -0500

    Add btn Functionality
    Able to identify which btn is the correct answer

--- character-game/components/characterGuess.jsx
+++ character-game/components/characterGuess.jsx
@@ -3,15 +3,30 @@ import _ from "lodash";
 import "./characterGuess.css";
 
 export default function CharacterGuess({ character, randomCharacters }) {
-
   console.log("character:", character);
-  
-  const combinedCharacters = [...randomCharacters, character].filter(char => char !== null);
+
+  const correctChar =  {...character, isSpecial: true};
+
+  const combinedCharacters = correctChar 
+  ? [...randomCharacters, correctChar] 
+  : [...randomCharacters].filter(
+    (char) => char !== null
+  );
 
   // Shuffle the combined array
   const shuffledCharacters = _.shuffle(combinedCharacters);
 
   console.log("shuffledCharacters:", shuffledCharacters);
+
+  //todo: add prop identifier to identify
+  //if button user has clicked is the correct character name
+  const handleClick= (char) => {
+    if (char.isSpecial) {
+      alert(`You clicked on the special character: ${char.name}`);
+    } else {
+      alert(`You clicked on: ${char.name}`);
+    }
+  };
 
   return (
     <div id="largeDiv">
@@ -33,15 +48,11 @@ export default function CharacterGuess({ character, randomCharacters }) {
             {shuffledCharacters.map(
               (char) =>
                 char && (
-                  <button key={char.id}>
+                  <button key={char.id} onClick={() => handleClick(char)}>
                     <li>{char.name}</li>
                   </button>
                 )
             )}
-            <button>
-              {" "}
-              <li>{character.name}</li>
-            </button>
           </ul>
         )}
       </div>
